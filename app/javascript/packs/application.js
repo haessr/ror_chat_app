6d5e5967387584579db95eb5a02a96666cecc127
Haess Romani <hromaza@gmail.com>
2021-04-22 10:56:25 -0500

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,14 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import "semantic-ui-sass";
 
-require("semantic-ui-sass");
+// require("semantic-ui-sass");
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+});
